36df947eae4906e371a8baaa530b1563ff69db2a
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2024-12-11 01:28:45 +0700

    Add Hamburger Menu

---
+++ src/script/openmenu.js
@@ -0,0 +1,10 @@
+function openMenu(){
+    var x = document.getElementById("menu");
+    x.style.display = "block";
+
+}
+
+function closeMenu(){
+    var x = document.getElementById("menu");
+    x.style.display = "none";
+}
