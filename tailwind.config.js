91c6fdf952c32427c34541ab742d5f29960f485a
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2024-12-16 11:47:36 +0700

    Setup games.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-purple': {
+          50: '#F2ECFC',   // Very light lavender
+          100: '#E4D9F9',
+          200: '#C9B5F1',
+          300: '#AE90E8',
+          400: '#9773DD',  // Slightly lighter than base
+          500: '#8465C4',  // Base color
+          600: '#6D53A3',
+          700: '#574182',
+          800: '#433261',
+          900: '#2F2242',  // Matches 'dark'
+          950: '#211631',  // Very dark
+        },
+      }
+    },
     fontFamily: {
       'main': ['Poiret One', 'sans-serif'],
     }
