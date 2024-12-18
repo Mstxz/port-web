5677e811e536473f55a18a1374111cac08e191b1
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2024-12-19 03:23:35 +0700

    Add info to game page + music page bug

--- src/script/playlist.js
+++ src/script/playlist.js
@@ -1,4 +1,15 @@
-document.getElementById("play-button").addEventListener("click", () => {
-    const audioPlayer = document.getElementById("audio-player");
-    audioPlayer.play();
+
+var playbuttons = document.querySelectorAll('.playbutton');
+
+function TriggerButton(event) {
+    var element = event.target;
+    if (element.className.includes('fa-play')) {
+        element.className = element.className.replace('fa-play', 'fa-pause');
+    } else {
+        element.className = element.className.replace('fa-pause', 'fa-play');
+    }
+}
+
+playbuttons.forEach(button => {
+    button.addEventListener('click', TriggerButton);
 });
