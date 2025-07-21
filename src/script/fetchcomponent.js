069a2fd349f6962f9c98f38bdcb8239fef4bebf2
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2025-07-21 14:37:03 +0700

    some more organized comp

--- src/script/fetchcomponent.js
+++ src/script/fetchcomponent.js
@@ -1,3 +1,4 @@
 fetch('components/header.html').then(res => res.text()).then(data => {document.getElementById('header').innerHTML = data;}); //header
 fetch('components/loader.html').then(res => res.text()).then(data => {document.getElementById('loader').innerHTML = data;}); //loader
 fetch('components/particle.html').then(res => res.text()).then(data => {document.getElementById('particle').innerHTML = data;});//particle
+fetch('components/menu.html').then(res => res.text()).then(data => {document.getElementById('menu').innerHTML = data;});//menu
