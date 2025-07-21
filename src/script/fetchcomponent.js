780e6a59f20b382621db34d6eaf41cd17b7da2b2
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2025-07-21 12:27:54 +0700

    some code fix for future edit

---
+++ src/script/fetchcomponent.js
@@ -0,0 +1,3 @@
+fetch('components/header.html').then(res => res.text()).then(data => {document.getElementById('header').innerHTML = data;}); //header
+fetch('components/loader.html').then(res => res.text()).then(data => {document.getElementById('loader').innerHTML = data;}); //loader
+fetch('components/particle.html').then(res => res.text()).then(data => {document.getElementById('particle').innerHTML = data;});//particle
