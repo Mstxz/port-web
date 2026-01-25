85a1146acaf447989227710ab995468f3c99a281
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2026-01-25 12:28:25 +0700

    a

--- src/script/utils/fetchcomponent.js
+++ src/script/utils/fetchcomponent.js
@@ -1,4 +1,4 @@
-fetch('/src/components/header.html').then(res => res.text()).then(comp => {
+fetch('/components/header.html').then(res => res.text()).then(comp => {
     let header = document.querySelector('header');
     header.classList = "flex justify-evenly items-center h-16 fixed w-full";
     header.innerHTML = comp;
