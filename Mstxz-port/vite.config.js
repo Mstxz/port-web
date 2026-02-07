bcc16a41655af59541249a788dfe2e22f16c6765
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2026-02-07 19:14:01 +0700

    fix deploy error

--- Mstxz-port/vite.config.js
+++ Mstxz-port/vite.config.js
@@ -2,15 +2,5 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    tailwindcss()
-  ],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        project: resolve(__dirname, 'page/project.html'),
-      }
-    }
-  }
-})
+  plugins: [tailwindcss()]
+});
