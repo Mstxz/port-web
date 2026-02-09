9dab1e787f79a5ac0d9be427dd0991e159f69a67
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2026-02-09 21:01:06 +0700

    manual revert

--- Mstxz-port/src/scripts/utils/theme.js
+++
@@ -1,9 +0,0 @@
-import './tailwind.css';
-
-document.querySelector('body').className = "font-primary bg-linear-to-b from-black to-ci-1 text-ci-3 bg-cover bg-center bg-fixed bg-no-repeat w-full h-screen overflow-x-hidden";
-
-fetch('/components/header.html').then(r => r.text()).then(comp => {
-    let header = document.querySelector('header');
-    header.className = "flex justify-evenly items-center fixed w-screen h-16 z-50 bg-linear-to-b from-black/50 to-transparent";
-    header.innerHTML = comp;
-});
