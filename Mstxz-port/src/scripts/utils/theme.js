047061ce7ea08e940cf0166125be5b52454d6424
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2026-02-09 21:23:44 +0700

    revert + change theme

---
+++ Mstxz-port/src/scripts/utils/theme.js
@@ -0,0 +1,3 @@
+let body = document.querySelector('body');
+
+body.className = "font-primary bg-linear-to-b from-ci-purple-1 to-ci-blue-1 text-ci-3 bg-cover bg-center bg-fixed bg-no-repeat w-full h-screen overflow-x-hidden";
