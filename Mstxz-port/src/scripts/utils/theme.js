5e0f7ec3558ebd2ebca61021aed1f1083d3599c3
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2026-02-09 22:45:26 +0700

    add primary fonts

--- Mstxz-port/src/scripts/utils/theme.js
+++ Mstxz-port/src/scripts/utils/theme.js
@@ -1,3 +1,2 @@
 let body = document.querySelector('body');
-
 body.className = "font-primary bg-linear-to-b from-ci-purple-1 to-ci-blue-1 text-ci-3 bg-cover bg-center bg-fixed bg-no-repeat w-full h-screen overflow-x-hidden";
