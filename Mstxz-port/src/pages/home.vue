87f739af9d527f2e34589e9913df81175cd9fff2
Mewadee Seeda <112562180+Mstxz@users.noreply.github.com>
2026-03-09 02:20:57 +0700

    fix responsive again

--- Mstxz-port/src/pages/home.vue
+++ Mstxz-port/src/pages/home.vue
@@ -50,34 +50,44 @@ import tools from '@/data/tools.json';
 
   <!-- About Section -->
   <section class="flex justify-center flex-col">
-      <div class="flex gap-4 sm:flex-col flex-col md:flex-row lg:flex-row">
-          <Contentcontainer class="w-1/2 sm:w-full">
-                <h1 class="font-bold text-2xl">About Me</h1>
-                <b>Full Name:</b> Mewadee Seeda<br>
-                <b>Education:</b>
-                <ul class="list-disc pl-8 py-2">
-                    <li>Undergraduate - B.Sc. (Information Technology), King Mongkut's Institute of Technology Ladkrabang</li>
-                </ul>
-                <b>Experiences:</b>
-                <ul class="list-disc pl-8 py-2">
-                    <li>2023 - 2025 | Teacher Director, Teacher Assistant @Hamsterhub.co</li>
-                    <li>2025 - Now | Co-Founder, Staff @GamiDev</li>
-                </ul>
-                <b>Skills:</b>
-                <ul class="list-disc pl-8 py-2">
-                    <li>Game Development</li>
-                    <li>Game Design</li>
-                    <li>Sound Engineer</li>
-                    <li>3D Artist</li>
-                    <li>UX/UI Design</li>
-                    <li>Front-end Web Development</li>
-                </ul>
-            </Contentcontainer>
-            <Contentcontainer class="w-1/2 sm:w-full md:w-1/2">
-                <h1 class="font-semibold text-2xl">Tech Stack</h1>
-                <toolbadge v-for="tool in tools" :key="tool.id" :icon="brandicon[tool.icon]" :id="tool.id"/>
-            </Contentcontainer>
-        </div>
+    <div class="flex flex-col md:flex-row gap-4">
+  
+  <Contentcontainer class="w-full md:w-1/2">
+    <h1 class="font-bold text-2xl">About Me</h1>
+    <b>Full Name:</b> Mewadee Seeda<br>
+    <b>Education:</b>
+    <ul class="list-disc pl-8 py-2">
+      <li>Undergraduate - B.Sc. (Information Technology), King Mongkut's Institute of Technology Ladkrabang</li>
+    </ul>
+
+    <b>Experiences:</b>
+    <ul class="list-disc pl-8 py-2">
+      <li>2023 - 2025 | Teacher Director, Teacher Assistant @Hamsterhub.co</li>
+      <li>2025 - Now | Co-Founder, Staff @GamiDev</li>
+    </ul>
+
+    <b>Skills:</b>
+    <ul class="list-disc pl-8 py-2">
+      <li>Game Development</li>
+      <li>Game Design</li>
+      <li>Sound Engineer</li>
+      <li>3D Artist</li>
+      <li>UX/UI Design</li>
+      <li>Front-end Web Development</li>
+    </ul>
+  </Contentcontainer>
+
+  <Contentcontainer class="w-full md:w-1/2">
+    <h1 class="font-semibold text-2xl">Tech Stack</h1>
+    <toolbadge
+      v-for="tool in tools"
+      :key="tool.id"
+      :icon="brandicon[tool.icon]"
+      :id="tool.id"
+    />
+  </Contentcontainer>
+
+</div>
   </section>
 </main>
 </template>
